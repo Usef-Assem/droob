9fe40574a6355ba197810960d61ac0533c12ccfa
Yousef Assem <usefassem96@gmail.com>
2024-12-12 19:19:34 +0200

    Updated ring animation with responsive scaling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import logo from '../../Assets/logo.png'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container">
         <a className="navbar-brand w-25" href="#">
-          <img src={logo} alt="droob" className='w-25' />
+          <img src={logo} alt="droob" className={style.logo} />
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
